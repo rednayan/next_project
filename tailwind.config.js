c522b5e87ce44653540c47d43f4b3a5cb14a3b47
nayan10102 <nayansharma1010@gmail.com>
2023-01-04 08:27:39 +0530

    feat: initial config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/_app.tsx",
+    "./pages/about.tsx",
+    "./pages/index.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
